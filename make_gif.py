1707c0139e6fc8483cfc811f0bc7640338c81bdc
Narumi <weaper@gmail.com>
2018-02-13 14:52:47 +0800

    add make_gif.py

---
+++ make_gif.py
@@ -0,0 +1,17 @@
+import imageio
+import glob
+import argparse
+from os.path import join
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dir', type=str)
+args = parser.parse_args()
+
+paths = sorted(glob.glob(join(args.dir, 'sample_*.jpg')))
+
+images = []
+for path in paths:
+    images.append(imageio.imread(path))
+
+imageio.mimsave(join(args.dir, 'sample.gif'), images)
+
